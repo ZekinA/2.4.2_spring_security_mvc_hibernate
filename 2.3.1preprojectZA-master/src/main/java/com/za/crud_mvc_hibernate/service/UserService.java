b62ab8884d0c628d2473a5e336fa81d95ea5068c
ZekinA <crabcore59@gmail.com>
2021-03-16 18:49:32 +0300

    Initial commit

---
+++ 2.3.1preprojectZA-master/src/main/java/com/za/crud_mvc_hibernate/service/UserService.java
@@ -0,0 +1,23 @@
+package com.za.crud_mvc_hibernate.service;
+
+import com.za.crud_mvc_hibernate.model.Role;
+import com.za.crud_mvc_hibernate.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    public List<User> getAllUsers();
+
+    public List<Role> getAllRoles();
+
+    public void saveUser(User user);
+
+    public void editUser(User user);
+
+    public User getUser(int id);
+
+    public User getUserByName(String username);
+
+    public void deleteUser(int id);
+}
